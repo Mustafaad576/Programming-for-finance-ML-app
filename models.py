b648048404b445921c96b0ba0d02f4bd839a8795
Mustafa Adnan <mustafaalt576@gmail.com>
2025-05-04 23:07:03 +0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+def train_model(X_train, y_train):
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def evaluate_model(model, X_test, y_test):
+    preds = model.predict(X_test)
+    mse = mean_squared_error(y_test, preds)
+    r2 = r2_score(y_test, preds)
+    return {"MSE": mse, "R2": r2}, preds
